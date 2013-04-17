68f42eae2038ae3d09ee46fda3388c838ebe7508
rolentle <rolentle@gmail.com>
2013-04-16 21:35:04 -0400

    fixed grammer errors in app.rb

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ get '/' do
     results = [ [teams[0], score[0] ], [teams[3], score[1] ] ]
 
     if results[0][0] == "Nationals" && results[0][1] > results[1][1]
-      @result_answer  = "Hell yeah, they beat #{results[1][0]}, #{results[0][1]} to #{results[1][1]}"
+      @result_answer  = "Hell yeah, they Nats #{results[1][0]}, #{results[0][1]} to #{results[1][1]}."
     elsif results[1][0] == "Nationals" && results[0][1] < results[1][1]
-      @result_answer = "Hell yeah, they beat #{results[0][0]}, #{results[1][1]} to #{results[0][1]}"
+      @result_answer = "Hell yeah, the Nats beat #{results[0][0]}, #{results[1][1]} to #{results[0][1]}."
     elsif results[0][0] == "Nationals" && results[0][1] < results[1][1]
-      @result_answer =  "Nah, they lost to the #{results[1][0]}, #{results[1][1]} to #{results[0][1]}"
+      @result_answer =  "Nope, the Nats lost to the #{results[1][0]}, #{results[1][1]} to #{results[0][1]}."
     elsif results[1][0] == "Nationals" && results[0][1] > results[1][1]
-      @result_answer = "Nah, they lost to the #{results[0][0]}, #{results[0][1]} to #{results[1][1]}"
+      @result_answer = "Nope, the Nats lost to the #{results[0][0]}, #{results[0][1]} to #{results[1][1]}."
     end
    
     return @result_answer
